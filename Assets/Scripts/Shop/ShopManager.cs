6b4ad0e26e37113bd1f5b17fa1f5c6418f042ef8
Trufafa <illmakeitworkthistime@gmail.com>
2026-05-20 00:28:34 +0100

    Minor Changes to Shop Layout, Also Created Timer Script from Scratch

---
+++ Assets/Scripts/Shop/ShopManager.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class ShopManager : MonoBehaviour
+{
+    [SerializeField] private Deck _currentDeck;
+    [SerializeField] private GameObject _cardOffer;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
